899e3a2f2b5f2316989c26462365f9c55cd2929d
mario32111 <marioge44@gmail.com>
2025-02-23 10:48:24 -0600

    first commmit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
