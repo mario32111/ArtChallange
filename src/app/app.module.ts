9031fc7cd486c17f81eb1a77246ee876424544c2
mario32111 <marioge44@gmail.com>
2025-03-23 10:30:23 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
@@ -11,7 +10,6 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     NotFoundComponent,
   ],
   imports: [
