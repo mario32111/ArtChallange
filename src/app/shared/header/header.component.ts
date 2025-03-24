97d1642b29abefa6526b64e510fb14c8cd3fe577
mario32111 <marioge44@gmail.com>
2025-03-23 22:51:28 -0600

    coleres del navbar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
