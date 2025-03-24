39af8f442d6ad7a0384795f91adc203a71619093
mario32111 <marioge44@gmail.com>
2025-03-23 20:11:00 -0600

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    HeaderComponent,
-  ],
+  /*   imports: [
+      HeaderComponent,
+    ], */
+
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
-}
+ }
