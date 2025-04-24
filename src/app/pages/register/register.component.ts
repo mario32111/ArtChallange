a09f568e75cf282d50ed75c917b7729264bff975
mario32111 <marioge44@gmail.com>
2025-04-24 01:05:16 -0600

    register component reactivo

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,22 +1,51 @@
 // register.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { FormsModule } from '@angular/forms'; // Importar FormsModule
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   standalone: true,
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
-  email: string = '';
-  password: string = '';
+export class RegisterComponent implements OnInit {
+  resultado!: string;
+  miClase: string = "msg1";
+
+  formularioValidacion = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    rePassword: new FormControl('', [Validators.required])
+  });
 
   constructor(private authService: AuthService) {}
 
+  ngOnInit(): void {
+    this.formularioValidacion.get('rePassword')?.addValidators(this.passwordValidator());
+
+    // Revalida rePassword cuando password cambie
+    this.formularioValidacion.get('password')?.valueChanges.subscribe(() => {
+      this.formularioValidacion.get('rePassword')?.updateValueAndValidity();
+    });
+  }
+
+
+  passwordValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      let password = this.formularioValidacion.get('password')?.value;
+      const rePassword = control.value;
+      return password === rePassword ? null : { 'passwordMissMatched': true };
+    };
+  }
+
+
+  //Funciones de autenticacion
   async loginWithGoogle() {
     try {
       const user = await this.authService.loginWithGoogle();
@@ -27,13 +56,24 @@ export class RegisterComponent {
   }
 
   async registerWithEmail() {
+    // Obtén los valores actuales del formulario
+    const { correo, password } = this.formularioValidacion.value;
+
+    if (!this.formularioValidacion.valid) {
+      this.resultado = "Formulario Invalido"
+      console.log(correo, password);
+      console.error('Correo o contraseña vacíos');
+      return;
+    }
+    this.resultado = "Formulario Valido"
+
     try {
-      const user = await this.authService.registerWithEmail(this.email, this.password);
+      const user = await this.authService.registerWithEmail(correo!, password!);
       console.log('Usuario registrado (Email):', user);
-      // Redirige al dashboard o muestra mensaje
+      // Redirige o muestra mensaje
     } catch (error) {
       console.error('Error al registrar usuario:', error);
-      // Aquí podrías mostrar un toast o alert
     }
   }
+
 }
